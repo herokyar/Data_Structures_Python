e2ffd13f68574b651cd221c68b66102f8ef7c05c
hasan erokyar <herokyar@gmail.com>
2015-04-15 13:06:14 -0700

    Create Hot_Potato_by_Queue.py

---
+++ Hot_Potato_by_Queue.py
@@ -0,0 +1,22 @@
+#famous hot potato problem , an algorithm using python queue data class
+
+from pythonds.basic.queue import Queue
+
+def hotPotato(namelist, num):
+    
+    simqueue = Queue() #create a new queue
+    
+    for name in namelist:
+        simqueue.enqueue(name)
+        
+    while simqueue.size() > 1 :
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue()) #the first in queue leaves the queue and enters again.
+        
+        #simqueue.dequeue()
+        break
+        
+    return simqueue.dequeue()
+
+#test the code
+hotPotato(["Bill","David","Susan","Jane","Kent","Brad"],1)
