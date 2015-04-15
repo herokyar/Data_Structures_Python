1ec3da8dc113af13cf0d0c23c8133ac7ab033612
hasan erokyar <herokyar@gmail.com>
2015-04-15 14:15:25 -0700

    Create Python_Deque.py

---
+++ Python_Deque.py
@@ -0,0 +1,25 @@
+
+#python deque data structure (combination of a stack and queue)
+
+class Deque:
+    
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+    
+    def addFront(self, item):
+        self.items.append(item)
+    
+    def addRear(self, item):
+        self.items.insert(0, item)
+        
+    def removeFront(self):
+        return self.items.pop()
+    
+    def removeRear(self):
+        return self.items.pop(0)
+    
+    def size(self):
+        return len(self.items)
