c28c14b201b39ba007653470ba1b2b126d66e771
hasan erokyar <herokyar@gmail.com>
2015-04-22 21:58:27 -0700

    Create Python_hash_table.py

---
+++ Python_hash_table.py
@@ -0,0 +1,89 @@
+#python hash table implementation (linear)
+#search complexity big-O(1) if there is no collision :)))
+
+class HashTable:
+    
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+        
+    def put(self,key,data):
+      hashvalue = self.hashfunction(key,len(self.slots))
+
+      if self.slots[hashvalue] == None:
+        self.slots[hashvalue] = key
+        self.data[hashvalue] = data
+      else:
+        if self.slots[hashvalue] == key:
+          self.data[hashvalue] = data  #replace
+        else:
+          nextslot = self.rehash(hashvalue,len(self.slots))
+          while self.slots[nextslot] != None and \
+                          self.slots[nextslot] != key:
+            nextslot = self.rehash(nextslot,len(self.slots))
+
+          if self.slots[nextslot] == None:
+            self.slots[nextslot]=key
+            self.data[nextslot]=data
+          else:
+            self.data[nextslot] = data #replace
+
+    def hashfunction(self,key,size):
+         return key%size
+
+    def rehash(self,oldhash,size):
+        return (oldhash+1)%size
+                
+
+    def get(self,key):
+      startslot = self.hashfunction(key,len(self.slots))
+
+      data = None
+      stop = False
+      found = False
+      position = startslot
+      while self.slots[position] != None and  \
+                           not found and not stop:
+         if self.slots[position] == key:
+           found = True
+           data = self.data[position]
+         else:
+           position=self.rehash(position,len(self.slots))
+           if position == startslot:
+               stop = True
+      return data
+
+    def __getitem__(self,key):
+        return self.get(key)
+
+    def __setitem__(self,key,data):
+        self.put(key,data)
+    
+    
+#test the code
+H = HashTable() #create a hash table
+
+H[54]="cat" #fill the hash table with key-value pairs
+H[26]="dog"
+H[93]="lion"
+H[17]="tiger"
+H[77]="bird"
+H[31]="cow"
+H[44]="goat"
+H[55]="pig"
+H[20]="chicken"
+
+H.slots
+H.data
+
+H[20] #look up a key-value pair
+H[17]
+H[20]='duck' #change the value 
+H[20]
+
+H.data
+
+print(H[99])
+
+
